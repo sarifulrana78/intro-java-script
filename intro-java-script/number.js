53c273dd6b8f6e1d3a71534ea547401938606dbc
Rana Bhai <sarifulislam536@gmail.com>
2026-01-23 01:05:31 +0600

    to learn fundamentals of JS Numbers

---
+++ intro-java-script/number.js
@@ -0,0 +1,11 @@
+var orangePrice =parseFloat("200.45");
+var choco =0-5;
+var ApplePrice =20;
+/* console.log(typeof orangePrice);
+console.log( orangePrice +ApplePrice ); */
+var first=.1;
+var second=.2;
+var total=first+second
+console.log(total.toFixed(3))
+const num=50/0
+console.log(num)
--- intro-java-script/types.js
+++ intro-java-script/types.js
@@ -4,6 +4,6 @@ console.log(isRich)
 var isRich = false;
 console.log(typeof isRich)
 
-
+/* string */
 var name ="rana";
 console.log(name)
