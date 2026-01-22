dd530071ba2ef77903dfb882157db52adb34769c
Rana Bhai <sarifulislam536@gmail.com>
2026-01-23 01:50:33 +0600

    task_2 complted

--- intro-java-script/mixed.js
+++ intro-java-script/mixed.js
@@ -1,10 +1,19 @@
-/* var first="rana"
+var first="rana"
 var second="islam"
 var fullname=first+second
-console.log(fullname) */
+console.log(fullname)
 
 var orangePrice=1000
 orangePrice -=700
 console.log(orangePrice)
+
+var mathe=parseFloat (75.25)
+var bio=65
+var chem= parseFloat (35.45)
+var phy=80
+var ban=parseFloat(99.50)
+var avg =(mathe+bio+chem+phy+ban) / 5
+
+console.log(avg.toFixed(2))
 
 
